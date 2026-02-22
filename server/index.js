b828c003ccb48fe37ff9e4cd5495c7e2881cc1f0
User <user@github.com>
2026-02-22 14:26:15 +0530

    Fix CORS origin for frontend-szk8

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://leadpilot-frontend-rg0m.onrender.com'],
+  origin: [
+    'http://localhost:3000',
+    'https://leadpilot-frontend-rg0m.onrender.com',
+    'https://leadpilot-frontend-szk8.onrender.com'
+  ],
   credentials: true
 }));
 app.use(express.json());
